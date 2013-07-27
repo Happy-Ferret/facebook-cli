7b59ff6ca33d8590b606466a55c88edbaabc5ef3
Gulshan Singh <gulshan@umich.edu>
2013-07-26 20:39:08 -0700

    Added listener to get textareas and pass content and class name to parse function

--- content.js
+++ content.js
@@ -1,3 +1,29 @@
+var input = "";
+
+$(document).ready(function(){
+	console.log("Document ready");
+
+	$('textarea').keyup(function(e) {
+		if (e.keyCode == 13) {
+			if (isCommand(this.value)) {
+				parse(input, this.className);
+			}
+			input = "";
+		} else{
+			input = this.value;
+		}
+	});
+});
+
+function isCommand(text) {
+	return true;
+}
+
+function parse(command, className) {
+	console.log(command);
+	console.log(className);
+}
+
 function exit() {
 }
 
