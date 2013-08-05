de57a51355fb211525c4114dd677668e2893175a
Gulshan Singh <gulshan@umich.edu>
2013-08-05 09:57:29 -0700

    Cleaned up code

--- content.js
+++ content.js
@@ -1,10 +1,6 @@
-var input = "";
 var token = "";
-var history = new Array();
-history[0] = "";
-var index = 0;
-var indexHigh = 0;
 
+// Listen for FB API auth token
 chrome.runtime.onMessage.addListener(
 	function(request, sender, sendResponse) {
 		token = request.token;
@@ -13,6 +9,14 @@ chrome.runtime.onMessage.addListener(
 
 $(document).ready(function() {
 	console.log("Document ready");
+
+	var input = "";
+	var index = 0;
+	var indexHigh = 0;
+	var history = new Array();
+	history[0] = "";
+
+	// Hijack all textareas
 	$(document).on("keyup", "textarea", function(e) {
 		if (e.keyCode == 13 && e.shiftKey) {
 			this.value = "";
@@ -39,79 +43,115 @@ $(document).ready(function() {
 	});
 });
 
-function isChatBox(className) {
-	return true;
+/**
+ * Parses a command
+ * @param  {Element} elem      The DOM element the command was typed into
+ * @param  {String}  command   The full command that was typed
+ * @param  {String}  className The classname of the DOM object
+ */
+function parse(elem, command, className) {
+	var firstSpace = command.indexOf(" ");
+	if (firstSpace != -1) {
+		var commandName = command.substr(0, firstSpace);
+		var args = command.substr(firstSpace + 1);
+	} else {
+		var commandName = command;
+		var args = "";
+	}
+	console.log(commandName);
+	console.log(args);
+
+	executeCommand(elem, commandName, args, className);
 }
 
-function parse(elem, command, className) {
-	var split_command = command.split(" ");
-	console.log(split_command);
-	var command = split_command[0]
-	if (split_command.length >= 2) {
-		var arg1 = split_command[1]
-	}
-	if (split_command.length == 3) {
-		var arg2 = split_command[2]
-	}
-
+/**
+ * Executes a command
+ * @param  {Element} elem 		 The DOM element that took in the input
+ * @param  {String}  commandName The command name
+ * @param  {String}  args        The full command that was typed without the
+ *                               command name
+ * @param  {String}  className   The classname of the DOM object
+ * @return {Object}              The command object
+ */
+function executeCommand(elem, commandName, args, className) {
 	if (isChatBox(className)) {
-		switch (command) {
-		case "min":
-			minimize(elem);
-			return;
-		case "exit":
-			exit(elem);
-			return;
-		case "n":
-			nextChat();
-			return;
-		case "p":
-			prevChat();
-			return;
-		case "cd":
-			changeDirectory(arg1);
-			return;
-		case "whoami":
-			whoami(elem);
-			return;
-	   }
-	}
+        switch (commandName) {
+        case "min":
+            minimizeChat(elem, args);
+            return;
+        case "exit":
+           	exitChat(elem, args);
+            return;
+        case "ssh":
+			ssh(elem, args);
+            return;
+        case "whoami":
+            whoAmI(elem, args);
+            return;
+       }
+    }
 
-	switch (command) {
+    switch (commandName) {
 	case "post":
-		post(arg1);
+		post(elem, args);
 		break;
 	case "chat":
-		chat(arg1, arg2);
-		break;
-	case "openprofile":
-		openProfile(arg1);
-		break;
-	case "profile":
-		openProfile();
-		break;
+		chat(elem, args);
+        break;
+	case "cd":
+        cd(elem, args);
+        break;
 	case "search":
-		search(arg1);
+		search(elem, args);
 		break;
 	default:
-		// TODO Display error to user
-		console.log("Command not found");
+		// TODO Alert if no chat window
+		console.log("Terminal not found");
 		printToTerminal("Command not found");
 	}
 }
 
-// Minimizes the chat dialogue box
-function minimize(elem) {
+/**
+ * Tells whether a command was typed in a chat box by looking at the className
+ * @param  {String}  className The classname of the DOM object
+ * @return {Boolean}           True if the DOM object is a chat box
+ */
+function isChatBox(className) {
+	return true;
+}
+
+/**
+ * Prints a message to the chat window
+ * @param  {String} message The message to print
+ */
+function printToTerminal(message) {
+	console.log(message);
+	var match = $(".conversation");
+	var $add = '<div class="mhs mbs pts fbChatConvItem _50dw clearfix small _50kd"><div class="_50ke"><div class="_50x5"></div></div><div class="messages"><div class="metaInfoContainer fss fcg"><span class="hidden_elem"><a href="#" rel="dialog" role="button"><span class="fcg">Report</span></a> · </span><span class="timestamp"></span></div><div class="_kso fsm direction_ltr _55r0" data-jsid="message" style="max-width: 188px;"><span data-measureme="1"><span id = "customMessage"></span></span></div></div></div>'
+	match.append($add);
+	$("#customMessage").append(message)
+	$("#customMessage").attr("id", "oldMessage");
+	$('.fbNubFlyoutBody.scrollable').scrollTop(100000);
+}
+
+/************************* Commands *************************/
+
+function minimizeChat(elem, args) {
 	var closest = $(elem).closest('.fbNub');
 	closest.removeClass('opened focusedTab');
 }
 
-// Closes the chat dialogue box
-function exit(elem) {
+function exitChat(elem, args) {
 	$('.close.button')[0].click();
 }
 
-function changeDirectory(directory) {
+function ssh(elem, args) {
+	// TODO
+}
+
+function cd(elem, args) {
+	var directory = args.split(" ")[0];
+
 	if (directory == "news") {
 		window.location.href = "https://www.facebook.com/";
 		return;
@@ -128,7 +168,7 @@ function changeDirectory(directory) {
 	window.location.href = redirect_url;
 }
 
-function whoami(elem) {
+function whoAmI(elem, args) {
 	var url = $("#pageNav > #navTimeline > a").attr("href");
 	var split_url = url.split('/');
 	var user_id = split_url[3];
@@ -140,22 +180,14 @@ function whoami(elem) {
 	$('.fbNubFlyoutBody.scrollable').scrollTop(100000);
 }
 
-function printToTerminal(message) { //Prints a message to the chat window
-	console.log(message);
-	var match = $(".conversation");
-	var $add = '<div class="mhs mbs pts fbChatConvItem _50dw clearfix small _50kd"><div class="_50ke"><div class="_50x5"></div></div><div class="messages"><div class="metaInfoContainer fss fcg"><span class="hidden_elem"><a href="#" rel="dialog" role="button"><span class="fcg">Report</span></a> · </span><span class="timestamp"></span></div><div class="_kso fsm direction_ltr _55r0" data-jsid="message" style="max-width: 188px;"><span data-measureme="1"><span id = "customMessage"></span></span></div></div></div>'
-	match.append($add);
-	$("#customMessage").append(message)
-	$("#customMessage").attr("id", "oldMessage");
-	$('.fbNubFlyoutBody.scrollable').scrollTop(100000);
-}
+function post(elem, args) {
+	status = args;
 
-function post(status) {
 	var url = "https://graph.facebook.com/me/feed?message=" + status;
 	console.log(url);
 	$.ajax(
 	{
-		type: 'POST',		
+		type: 'POST',
 		url: url,
 		data: {access_token: token},
 		success: function (data) { console.log("success:"); console.log(data); },
@@ -163,13 +195,11 @@ function post(status) {
 	});
 }
 
-function nextChat() {
-}
+function chat(elem, args) {
+	args = args.split(" ");
+	firstname = args[0];
+	lastname = args[1];
 
-function prevChat() {
-}
-
-function chat(firstname, lastname) {
 	var username = firstname + " " + lastname;
 	var elem = $("._42fz a .clearfix ._52zl:contains('"+username+"')");
 	console.log(elem);
@@ -182,11 +212,6 @@ function chat(firstname, lastname) {
 	}
 }
 
-function openProfile() {
-}
-
-function openProfile(userName) {
-}
-
-function search(query) {
+function search(elem, args) {
+	// TODO
 }
